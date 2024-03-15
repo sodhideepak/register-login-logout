2756ca11edc0140c4b6b136c516ca2eb91842a0d
deepak sodhi <deepaksodhi0023@gmail.com>
2024-03-15 23:38:41 +0530

    files upload cancel

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -19,17 +19,17 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router =Router()
 
 router.route("/register").post(
-    upload.fields([
-        {
-        name:"avatar",
-        maxCount:1
-        },
-        {
-        name:"coverimage",
-        maxCount:1
+    // upload.fields([
+    //     {
+    //     name:"avatar",
+    //     maxCount:1
+    //     },
+    //     {
+    //     name:"coverimage",
+    //     maxCount:1
         
-        }
-    ]),
+    //     }
+    // ]),
     registeruser)
 
 router.route("/login").post(loginuser)
